c8d69c6df169f85e73589932098f486d43fe5aef
jrong04 <143994205+jrong04@users.noreply.github.com>
2024-08-07 14:19:35 +0800

    N2U-Payroll 07/08/2024
    -Added validator to shift time

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Mail;
+use Carbon\Carbon;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -28,5 +30,26 @@ class AppServiceProvider extends ServiceProvider
             Mail::alwaysTo('developer@currenttech.pro');
         }
 
+        Validator::extend('shift_end_after_start', function ($attribute, $value, $parameters, $validator) {
+            // $value is the value of shift_end
+            // $parameters[0] is the value of shift_start
+            $shiftStart = $parameters[0];
+            $shiftEnd = $value;
+
+            if (!preg_match('/^\d{2}:\d{2}$/', $shiftStart) || !preg_match('/^\d{2}:\d{2}$/', $shiftEnd)) {
+                return false; // Return false if time format is invalid
+            }
+
+            // Convert times to Carbon objects for easier comparison
+            $startTime = Carbon::createFromFormat('H:i', $shiftStart);
+            $endTime = Carbon::createFromFormat('H:i', $shiftEnd);
+
+            // Check if shift_end is after shift_start or if shift_end is past midnight and shift_start is in the evening
+            return $endTime->greaterThan($startTime) || ($endTime->lessThan($startTime) && $endTime->greaterThan($startTime->copy()->addHours(4)));
+        });
+
+        Validator::replacer('shift_end_after_start', function ($message, $attribute, $rule, $parameters) {
+            return str_replace(':shift_start', $parameters[0], $message);
+        });
     }
 }
